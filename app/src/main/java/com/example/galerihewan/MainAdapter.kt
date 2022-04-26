ed02175fc2dd828dd5daf2de98b3bce732c73fbc
cahyana78 <49891412+cahyana78@users.noreply.github.com>
2022-04-26 22:46:08 +0700

    perubahan terakhir modul04

---
+++ app/src/main/java/com/example/galerihewan/MainAdapter.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2022 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 1.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.example.galerihewan
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+
+
+
+class MainAdapter(
+    private val data: List<Hewan>
+) : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ListItemBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    class ViewHolder(
+        private val binding: ListItemBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(hewan: Hewan) = with(binding) {
+            namaTextView.text = hewan.nama
+            latinTextView.text = hewan.namaLatin
+            imageView.setImageResource(hewan.imageResId)
+
+            root.setOnClickListener {
+                val message = root.context.getString(R.string.message, hewan.nama)
+                Toast.makeText(root.context, message, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
