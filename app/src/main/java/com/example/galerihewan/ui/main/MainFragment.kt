69f90f13554a5d3f595d06444120d739e4065af9
cahyana78 <49891412+cahyana78@users.noreply.github.com>
2022-04-27 01:17:06 +0700

    mengubah ke tampilan grid

--- app/src/main/java/com/example/galerihewan/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/galerihewan/ui/main/MainFragment.kt
@@ -1,12 +1,13 @@
 package com.example.galerihewan.ui.main
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.galerihewan.model.Hewan
 import com.example.galerihewan.R
@@ -15,6 +16,8 @@ import com.example.galerihewan.databinding.FragmentMainBinding
 
 
 class MainFragment : Fragment(){
+
+    private var isLinearLayoutManager = true
 
     private val viewModel: MainViewModel by lazy {
         ViewModelProvider(this).get(MainViewModel::class.java)
@@ -37,6 +40,8 @@ class MainFragment : Fragment(){
             setHasFixedSize(true)
         }
 
+        setHasOptionsMenu(true)
+
         return binding.root
     }
 
@@ -46,5 +51,54 @@ class MainFragment : Fragment(){
         viewModel.getData().observe(viewLifecycleOwner, {
             myAdapter.updateData(it)})
     }
-    
+
+    private fun chooseLayout() {
+        if (isLinearLayoutManager) {
+            binding.recyclerView.layoutManager = LinearLayoutManager(this.requireContext())
+        } else {
+            binding.recyclerView.layoutManager = GridLayoutManager(this.requireContext(), 2)
+        }
+
+    }
+
+    private fun setIcon(menuItem: MenuItem?) {
+        if (menuItem == null)
+            return
+
+        // Set the drawable for the menu icon based on which LayoutManager is currently in use
+
+        // An if-clause can be used on the right side of an assignment if all paths return a value.
+        // The following code is equivalent to
+        // if (isLinearLayoutManager)
+        //     menu.icon = ContextCompat.getDrawable(this, R.drawable.ic_grid_layout)
+        // else menu.icon = ContextCompat.getDrawable(this, R.drawable.ic_linear_layout)
+        menuItem.icon =
+            if (isLinearLayoutManager)
+                ContextCompat.getDrawable(this.requireContext(), R.drawable.ic_grid_layout)
+            else ContextCompat.getDrawable(this.requireContext(), R.drawable.ic_linear_layout)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.layout_menu, menu)
+
+        val layoutButton = menu?.findItem(R.id.action_switch_layout)
+        // Calls code to set the icon based on the LinearLayoutManager of the RecyclerView
+        setIcon(layoutButton)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_switch_layout -> {
+                // Sets isLinearLayoutManager (a Boolean) to the opposite value
+                isLinearLayoutManager = !isLinearLayoutManager
+
+                // Sets layout and icon
+                chooseLayout()
+                setIcon(item)
+
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
